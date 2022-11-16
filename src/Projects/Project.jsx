a9301966770fbbb8dae00d2523101819ca6fe5c1
ER-Mollo <mollorefilwe68@gmail.com>
2022-11-16 17:08:10 +0200

    slide-added

--- src/Projects/Project.jsx
+++ src/Projects/Project.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+
 import "./Project.scss";
+import { Zoom } from 'react-slideshow-image';
+
 import image1 from '../assets/image2.png'
 import image2 from '../assets/image3.png'
 import image3 from '../assets/image4.png'
@@ -20,24 +23,40 @@ function Projects() {
             <h2>Recent Projects</h2>
             <h1>Look At The Projects I Worked on</h1>
          </div>
-        
-         <Slide>
+         <div className='slide'>
+         <Zoom className="slide">
             <div className="each-slide-effect">
-                <div style={{ 'backgroundImage': `url(${images[0]})` }}>
+                <div style={{ 'backgroundImage': `url(${images[0]})`, 'height':'100vh' }}>
                     <span>Slide 1</span>
                 </div>
             </div>
             <div className="each-slide-effect">
-                <div style={{ 'backgroundImage': `url(${images[1]})` }}>
+                <div style={{ 'backgroundImage': `url(${images[1]})`, 'height':'100vh'  }}>
                     <span>Slide 2</span>
                 </div>
             </div>
             <div className="each-slide-effect">
-                <div style={{ 'backgroundImage': `url(${images[2]})` }}>
+                <div style={{ 'backgroundImage': `url(${images[2]})`, 'height':'100vh'  }}>
                     <span>Slide 3</span>
                 </div>
             </div>
-        </Slide>
+        </Zoom>
+
+         </div>
+        
+         
+
+         <div className="project-content">
+            <div style={{ 'backgroundImage': `url(${images[0]})`,'height':'100vh' }} className="select">
+                   <span>Slide 1</span>
+             </div>
+             <div style={{ 'backgroundImage': `url(${images[1]})` }} className="select">
+                   <span>Slide 2</span>
+             </div>
+             <div style={{ 'backgroundImage': `url(${images[2]})` }} className="select">
+                   <span>Slide 3</span>
+             </div>   
+         </div>
 
          
       </div>
